1dde3fed38b40d517f75bc72ffecb707cf39b36f
10zinnyims <tnyima@appdev.yearup.org>
2023-04-21 12:56:22 -0400

    added new files

---
+++ src/main/java/DiceRoll.java
@@ -0,0 +1,10 @@
+public class DiceRoll {
+    public static void main(String[] args) {
+        int[] diceValues = new int[20];
+        int count2 = 0;
+        int count4 = 0;
+        int count6 = 0;
+        int count7 = 0;
+
+    }
+}
---
+++ src/main/java/WhileLoop.java
@@ -0,0 +1,17 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 1;
+
+        while (count <= 5) {
+            System.out.println("I love Java");
+            count++;
+        }
+        int count2 = 1;
+        do {
+            System.out.println("I love Java");
+            count2++;
+
+        } while (count2 <= 5);
+    }
+}
+
